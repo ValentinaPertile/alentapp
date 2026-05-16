d480cfa725422c1defa44df9089f624865e4b07a
Luana <luana.suarez@alu.frlp.utn.edu.ar>
2026-05-16 01:02:16 +0000

    feat(locker): agrega acceso a lockers en Home

--- packages/web/src/views/Home.tsx
+++ packages/web/src/views/Home.tsx
@@ -1,5 +1,5 @@
 import { Box, SimpleGrid, Heading, Text, VStack } from "@chakra-ui/react";
-import { LuUsers } from "react-icons/lu";
+import { LuUsers, LuArchive } from "react-icons/lu";
 import { SectionCard } from "../components/SectionCard";
 
 export function HomeView() {
@@ -29,6 +29,12 @@ export function HomeView() {
           description="Administra el padrón de socios, sus categorías, estados de cuenta y datos personales."
           to="/members"
           icon={LuUsers}
+        />
+        <SectionCard
+          title="Casilleros"
+          description="Gestiona el alta, modificación, disponibilidad y baja lógica de los casilleros del club."
+          to="/lockers"
+          icon={LuArchive}
         />
 
         {/* Future sections can be added here following the same pattern */}
